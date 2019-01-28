95f1c044f2478463e27699d1172b49196baa9f79
nvarabioff <46659008+nvarabioff@users.noreply.github.com>
2019-01-27 22:56:17 -0500

    Update lab1_polynomial.hpp

--- lab1_polynomial.hpp
+++ lab1_polynomial.hpp
@@ -19,31 +19,103 @@ class Polynomial {
 	int data_size;
 	friend class PolynomialTest;
 public:
-	Polynomial(int A[], int size);	//class constructor with 2 input parameters
-	Polynomial();					//class constructor with random values
-	Polynomial(string fileName);	//class constructor with input file
-	~Polynomial();					//destructor performs cleanup if needed
+	Polynomial(int A[], int size);	
+	//PURPOSE: class constructor with 2 input parameters
+		//INPUTS: Array A, int size
+		//RETURNS: Polynomial Object
 	
-	bool operator==(const Polynomial& target);		//performs *this == target
-	void print();									//prints the polynomial (ex. 2x^3 + 1x^0)
-	Polynomial operator+(const Polynomial& target);	//performs *this + target
-	Polynomial operator-(const Polynomial& target);	//performs *this - target
-	Polynomial operator*(const Polynomial& target);	//performs *this * target
-	Polynomial derivative();						//computes the derivative d/dx of *this
+	Polynomial();
+	//PURPOSE: class constructor with random values
+		//INPUTS: none
+		//RETURNS: Polynomial Object
 	
+	Polynomial(string fileName);
+	//PURPOSE: class constructor with input file
+		//INPUTS: name of a file as a string
+		//RETURNS: Polynomial Object
+
+	~Polynomial();
+	//PURPOSE: destructor performs cleanup if needed
+		//INPUTS: none
+		//RETURNS: none
 	
+	bool operator==(const Polynomial& target);
+	//PURPOSE: performs *this == target
+		//INPUTS: 2 Polynomial objects
+		//RETURNS: boolean value of equal or not equal
+	
+	void print();
+	//PURPOSE: prints the polynomial object (ex. 2x^3 + 1x^0)
+		//INPUTS: none
+		//RETURNS: none
+
+	Polynomial operator+(const Polynomial& target);
+	//PURPOSE: performs *this + target
+		//INPUTS: 2 Polynomial objects
+		//RETURNS: Polynomial object result
+
+	Polynomial operator-(const Polynomial& target);
+	//PURPOSE: performs *this - target
+		//INPUTS: 2 Polynomial objects
+		//RETURNS: Polynomial object result
+		
+	Polynomial operator*(const Polynomial& target);
+	//PURPOSE: performs *this * target
+		//INPUTS: 2 Polynomial objects
+		//RETURNS: Polynomial object result
+		
+	Polynomial derivative();
+	//PURPOSE: computes the derivative d/dx of *this
+		//INPUTS: none
+		//RETURNS: Polynomial object result
+	
+	int getDegree();
+	//PURPOSE: get the data_size (degree) member of Polynomial object
+		//INPUTS: none
+		//RETURNS: int data_size of Polynomial object
 };
 
 class PolynomialTest {
 public:
-	bool test_constructors();	//Tests the constructors
-	bool test_isEqual();		//Tests '==' operator
-	bool test_addition();		//Tests '+' operator
-	bool test_subtraction();	//Tests '-' operator
-	bool test_multiplication();	//Tests '*' operatop
-	bool test_derivative();		//Tests derivative function
-	void test_print();			//Tests print function
-	void run();					//Runs all tests
+	bool test_constructors();
+	//PURPOSE: Tests the constructors
+		//INPUTS: none
+		//RETURNS: bool value of test successful
+		
+	bool test_isEqual();
+	//PURPOSE: Tests '==' operator
+		//INPUTS: none
+		//RETURNS: bool value of test successful
+		
+	bool test_addition();
+	//PURPOSE: Tests '+' operator
+		//INPUTS: none
+		//RETURNS: bool value of test successful
+		
+	bool test_subtraction();
+	//PURPOSE: Tests '-' operator
+		//INPUTS: none
+		//RETURNS: bool value of test successful
+		
+	bool test_multiplication();
+	//PURPOSE: Tests '*' operatop
+		//INPUTS: none
+		//RETURNS: bool value of test successful
+		
+	bool test_derivative();
+	//PURPOSE: Tests derivative function
+		//INPUTS: none
+		//RETURNS: bool value of test successful
+		
+	void test_print();
+	//PURPOSE: Tests print function
+		//INPUTS: none
+		//RETURNS: none
+		
+	void run();
+	//PURPOSE: Runs all tests
+		//INPUTS: none
+		//RETURNS: none
 };
 
 #endif
